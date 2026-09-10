e76fbe6614383afe5e0a33c5fc25704eda337d4f
Kimkimseeun <riwoo.03@gmail.com>
2026-09-11 00:24:12 +0900

    Record discovery and preference update 329

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE="seorap-tasks-v88";
+const CACHE="seorap-tasks-v89";
 const CORE=["./","./index.html","./styles.css?v=22","./data.js?v=80","./app.js?v=22","./manifest.webmanifest"];
 self.addEventListener("install",event=>event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(CORE)).then(()=>self.skipWaiting())));
 self.addEventListener("activate",event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE).map(key=>caches.delete(key)))).then(()=>self.clients.claim())));
