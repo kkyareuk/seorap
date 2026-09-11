826e72053a27354e432abbec25a5b49eec245b53
Kimkimseeun <riwoo.03@gmail.com>
2026-09-12 05:32:33 +0900

    Confirm Apple accepted restore follow-up build362

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE="seorap-tasks-v131";
+const CACHE="seorap-tasks-v132";
 const CORE=["./","./index.html","./styles.css?v=22","./data.js?v=124","./app.js?v=22","./manifest.webmanifest"];
 self.addEventListener("install",event=>event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(CORE)).then(()=>self.skipWaiting())));
 self.addEventListener("activate",event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE).map(key=>caches.delete(key)))).then(()=>self.clients.claim())));
