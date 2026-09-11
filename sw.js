f2fba35ee3714f620cd3ff4b1d8b78b24d91ed8f
Kimkimseeun <riwoo.03@gmail.com>
2026-09-11 19:54:26 +0900

    Record Android 344 performance measurements and delivered artifacts

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE="seorap-tasks-v106";
+const CACHE="seorap-tasks-v107";
 const CORE=["./","./index.html","./styles.css?v=22","./data.js?v=98","./app.js?v=22","./manifest.webmanifest"];
 self.addEventListener("install",event=>event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(CORE)).then(()=>self.skipWaiting())));
 self.addEventListener("activate",event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE).map(key=>caches.delete(key)))).then(()=>self.clients.claim())));
